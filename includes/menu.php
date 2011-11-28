993424c1a6e1b5d027a24215b782a481ba8a57b1
Keith Hallock <krhallock@plymouth.edu>
2011-11-28 14:50:00 -0500

    ksdjf

--- includes/menu.php
+++ includes/menu.php
@@ -5,13 +5,13 @@ $root = "http://".$_SERVER['SERVER_NAME'];
 ?>
 
 <ul>
-	<li> <a href=<?php $root ?>/~krhallock/RYM/index.php> Home </a> 				</li>
+	<li> <a href=<?php $root ?>/~krhallock/Rate-Your-Mate/index.php> Home </a> 				</li>
 
-	<li> <a href="<?php echo $root; ?>/~krhallock/RYM/instruct/project_setup.php"> Instructor Setup </a> 	</li>
-	<li> <a href="<?php echo $root; ?>/~krhallock//RYM/student/create_contract.php"> Create a Contract </a> 	</li>
+	<li> <a href="<?php echo $root; ?>/~krhallock/Rate-Your-Mate/instruct/project_setup.php"> Instructor Setup </a> 	</li>
+	<li> <a href="<?php echo $root; ?>/~krhallock/Rate-Your-Mate/student/create_contract.php"> Create a Contract </a> 	</li>
 
-	<li> <a href="<?php echo $root; ?>/~krhallock/RYM/student/student_input.php"> Student Input </a> 		</li>
-	<li> <a href="<?php echo $root; ?>/~krhallock/RYM/student/evaluatee_student.php"> Student Evaluatee </a>	</li>
-	<li> <a href="<?php echo $root; ?>/~krhallock/RYM/instruct/evaluatee_instructor.php" > Instructor Evaluation </a>	</li>
+	<li> <a href="<?php echo $root; ?>/~krhallock/Rate-Your-Mate/student/student_input.php"> Student Input </a> 		</li>
+	<li> <a href="<?php echo $root; ?>/~krhallock/Rate-Your-Mate/student/evaluatee_student.php"> Student Evaluatee </a>	</li>
+	<li> <a href="<?php echo $root; ?>/~krhallock/Rate-Your-Mate/instruct/evaluatee_instructor.php" > Instructor Evaluation </a>	</li>
 </ul>
 
