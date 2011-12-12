a9c0fd9bbe171f759fa69df1e8a41eed2b554d31
Kris Reynolds <krmreynolds@gmail.com>
2011-12-12 11:56:04 -0500

    Adding some of the core functionality

--- instruct/evaluatee_instructor.php
+++ instruct/evaluatee_instructor.php
@@ -16,6 +16,11 @@ if ( !empty( $_POST['groupID'] ) ) {
 		$_SESSION['studentID'] = $_POST['studentSelect'];
 	}
 }
+if ( !empty( $_POST['evalType'] )) {
+	$evalType= 'source';
+	echo $_POST['evalType'];
+}
+else $evalType = 'target';
 ?>
 <html>
 <head>
@@ -35,20 +40,15 @@ if ( !empty( $_POST['groupID'] ) ) {
 
 <div id="content">
 	<form id='groupSelectForm' name='form' action="evaluatee_instructor.php" method="post">
-	<?php 
-        // Get all groups for this project
-        $groupIDQuery = mysql_query ( " SELECT DISTINCT G.GrpID FROM Groups G WHERE
-	        G.PrjID = ".$prjID);
-	
-	if ( $groupIDQuery ) {
-	?>
 	<select name="groupID" onchange='this.form.submit()'>	
 		<?php
+		// Get all groups for this project
+		$groupIDQuery = mysql_query ( " SELECT DISTINCT G.GrpID FROM Groups G WHERE
+			G.PrjID = ".$prjID) or die( 'Error could not retrieve the group list' );
 		$cnt = 1;
 		// Select the group from the project to review, all groups have a default name
 		while ( $groupID = mysql_fetch_array ( $groupIDQuery ) ) {
 			$groupIDArr[$cnt] = $groupID['GrpID'];
-			// If the session is empty or coming from a different page, reassin the session
 			if ( empty( $_SESSION['groupID'] ) || empty( $_POST['groupID'] ) ) 
 				$_SESSION['groupID'] = $groupID['GrpID'];
 			if ( $_SESSION['groupID'] == $groupID['GrpID'] )
@@ -58,9 +58,7 @@ if ( !empty( $_POST['groupID'] ) ) {
 		}
 		?>	
 	</select>
-	<?php }
-	else echo 'Could not retrieve group list';
-	?>
+
 	<select name="studentSelect" onchange='this.form.submit()'>	
 		<?php
 		// Get the ID's of all group members
@@ -69,23 +67,26 @@ if ( !empty( $_POST['groupID'] ) ) {
 		
 		$cnt = 0;	
 		// Select the student from the group to be reviewed
-		while ( $studentID = mysql_fetch_array( $groupSdtIDQuery ) ) {	
+		while ( $studentID = mysql_fetch_array( $groupSdtIDQuery ) ) {
+			
 			foreach ( $_SESSION['roster'] as $student ) {
+				if ( empty( $_SESSION['studentID'] ) || $groupChangeFlag == 1 || empty( $_POST['groupID'] ) )
+					$_SESSION['studentID'] = $student['id'];
+				if ( $_SESSION['studentID'] == $student['id'] ) {
+					$defaultString = 'selected="selected"';
+					$studentName = $student['name'];
+				}
+				else $defaultString = '';
 				if ( $student['id'] == $studentID['StudentID'] ) {
-					if ( empty( $_SESSION['studentID'] ) || $groupChangeFlag == 1 || empty( $_POST['groupID'] ) )
-						$_SESSION['studentID'] = $student['id'];
-					if ( $_SESSION['studentID'] == $student['id'] ) {
-						$defaultString = 'selected="selected"';
-						$studentName = $student['name'];
-					}
-					else $defaultString = '';
 					$groupArr[$student['id']] = $student['name'];
 					echo '<option value = ' . $student['id'] . ' ' . $defaultString . '> ' . $student['name'] . '</option>';
 				}
 			}
 		}
 		?> 
-	</select> 
+	</select>
+	<input type="checkbox" id="evalCheck" name="evalType" value="howdy" onchange='this.form.submit()' 
+		<?php if( $evalType == 'source' ) echo 'checked'; ?>/> Evaluations by this student 
 	</form>
 		
 	<h4>Below is the feedback for <?php echo $studentName; ?> from their teammates </h4>
@@ -100,43 +101,34 @@ if ( !empty( $_POST['groupID'] ) ) {
 	if ( $behaviorQuery ) {
 		$behaviorCnt = mysql_num_rows( $behaviorQuery );
 	}
-	else $behaviorCnt = -1;
+	else $behaviorCnt =
 	// Put into an array
 	$cnt = 0;
 
 	// Only try to display comments if the contract exists
 	if ( $behaviorCnt != 0 ) {
-		while ( $behavior = mysql_fetch_array( $behaviorQuery ) ) {
-			$behaviorArr[ $behavior['BehaviorID'] ] = $behavior['Description'];
-		}
+	while ( $behavior = mysql_fetch_array( $behaviorQuery ) ) {
+		$behaviorArr[ $behavior['BehaviorID'] ] = $behavior['Description'];
+	}
 	
-		// Get all comments related to this student for this project	
-		$commentQueryString = 'SELECT C.SrcId, C.Comment, C.BehaviorId FROM Comments C 
-			WHERE C.PrjID = ' . $_SESSION['prjID'] . ' AND C.TargetId = ' . $_SESSION['studentID'] . ' ORDER BY C.BehaviorId;';
-		
-		$commentQuery = mysql_query( $commentQueryString );
-		if ( $commentQuery ) { 
-			// Get the number of rows
-			$commentCnt = mysql_num_rows( $commentQuery );
-			if ( $commentCnt <= 0 )
-				echo '<p> No comments have been left for this student </p>';
-		} else $commentCnt = -1;
-		// Print out the behavior
-		$cnt = 0;
-		if ( $commentCnt != -1 ) {
-		while ( $comment = mysql_fetch_array( $commentQuery ) ) {
-			// If this is the first for this behavior, create a header
-			if ( empty( $currBehavior ) || $currBehavior != $comment['BehaviorId'] ) {
-				echo '<h4> ' . $behaviorArr[ $comment['BehaviorId'] ] . '</h4>';
-				$currBehavior = $comment['BehaviorId'];
-			}
-			// Print out the student who wrote the review
-			echo '<p> Source: ' . $groupArr[$comment['SrcId'] ]. '</p>';
-			// Print out the comment
-			echo '<textarea> ' . trim( $comment['Comment'] ) . '</textarea>';
+	// Get all comments related to this student for this project	
+	$commentQueryString = 'SELECT C.SrcId, C.Comment, C.BehaviorId FROM Comments C 
+		WHERE C.PrjID = ' . $_SESSION['prjID'] . ' AND C.TargetId = ' . $_SESSION['studentID'] . ' ORDER BY C.BehaviorId;';
+	$commentQuery = mysql_query( $commentQueryString ) or die ( 'Could not find student comments' );
+
+	// Print out the behavior
+	$cnt = 0;
+	while ( $comment = mysql_fetch_array( $commentQuery ) ) {
+		// If this is the first for this behavior, create a header
+		if ( empty( $currBehavior ) || $currBehavior != $comment['BehaviorId'] ) {
+			echo '<h4> ' . $behaviorArr[ $comment['BehaviorId'] ] . '</h4>';
+			$currBehavior = $comment['BehaviorId'];
 		}
-		} 
-		
+		// Print out the student who wrote the review
+		echo '<p> Source: ' . $groupArr[$comment['SrcId'] ]. '</p>';
+		// Print out the comment
+		echo '<textarea> ' . trim( $comment['Comment'] ) . '</textarea>';
+	}
 	}
 	else { ?>
 		<p> No contract was found for this group! </p>
