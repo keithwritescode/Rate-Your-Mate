c3340895bc76cfa53be5e56bdab8aaf89b722e12
Tyler Ferm <tfermm@gmail.com>
2011-12-13 22:10:48 -0500

    fixed numbers on piechart

--- includes/piechart/setup.php
+++ includes/piechart/setup.php
@@ -28,50 +28,17 @@
 	$row = mysql_fetch_assoc($result);	
 	$maxPoints = $row['totalPoints'];
 	
-	sessionRoll();
-	
-function sessionRoll()
-{
-	$username = "jadennett";
-	$password = "hensvolk";
-	$hostname = "turing"; 
-	$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
-	$selected = mysql_select_db("wp1",$dbhandle) or die("Could not select examples");
-	$result = mysql_query("SELECT DISTINCT a.roleid as roleid, b.id as id, b.firstname as firstname, b.lastname as lastname 
-							FROM mdl_role_assignments a, mdl_user b 
-							WHERE b.username='{$_SESSION['username']}'");
-	
-	$row = mysql_fetch_assoc($result);
-	if ($row['roleid'] = 3)
+	if ($_SESSION['userType'] == 'student' || $_SESSION['userType'] == 'guest')
 	{
-		$_SESSION['userType'] = 'instructor';
+		$teacher = 0;
 	}
-	else if ($row['roleid'] = 1)
-	{
-		$_SESSION['userType'] = 'admin';
-	}
-	else if ($row['roleid'] = 2)
-	{
-		$_SESSION['userType'] = 'admin';
-	}
-	else if ($row['roleid'] = 4)
-	{
-		$_SESSION['userType'] = 'instructor';
-	}
-	else if ($row['roleid'] = 5)
-	{
-		$_SESSION['userType'] = 'student';
-	}
-	else if ($row['roleid'] = 6)
-	{
-		$_SESSION['userType'] = 'guest';	
-	}
-}
-	//print_r ($row);
-	
-	if ($_SESSION['userType'] <> 5 && $_SESSION['userType'] <> 6)
+	else 
 	{
 		$teacher = 1;
 	}
+	
+	?>
+	
+	
 	
 	
